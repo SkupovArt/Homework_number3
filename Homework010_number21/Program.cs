0b778f5717f0ce247398e353ee6c3cce09a671c9
SkupovArt <skripachmaster39@gmail.com>
2022-07-21 14:31:08 +0400

    Begin

---
+++ Homework010_number21/Program.cs
@@ -0,0 +1,29 @@
+﻿// // 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// AB = √ (x b — x a) 2 + (y b — y a) 2 + (z b — z a) 
+
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки А: x1 =  ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А: y1 =  ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А: z1 =  ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: x2 =  ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: y2 =  ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: z2 =  ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+//Math.Round(N, 2); округление
+// Math.Sqrt(x); квадратный корень
+//Math.Pow(x,y); возвести число x в степень y
+double Line = Math.Round((Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2))), 2);
+
+
+Console.WriteLine($"Расстояние между точками = {Line}");
---
+++ Homework012_number19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//Console.Clear();
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] pal = {(int)num};
+void Palindrome(int[] pal)
+{
+    int count = pal.Length;
+
+    for (int i = 0; i < count/2; i++)
+    {
+       for (int j = count; j > i; j--)
+       {
+            if (i == j) Console.WriteLine("Ваше число палиндром");
+           else  Console.WriteLine("ваше число не палиндром");
+    
+       }
+    }
+}
+
+Palindrome(pal);
