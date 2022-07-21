3b0fc4c91f703759065367d8ecd6bdabc45b392e
SkupovArt <skripachmaster39@gmail.com>
2022-07-21 15:18:19 +0400

    С кровью и потом

--- Homework012_number19/Program.cs
+++ Homework012_number19/Program.cs
@@ -4,23 +4,18 @@
 // 12821 -> да
 // 23432 -> да
 
-//Console.Clear();
-Console.WriteLine("Введите число:  ");
+//Долго бодался с этой задачей, в итоге пришел к мнению, что проще отразить введенное слово 
+// или число и сравнить исходник с ревесом
+// Но возникли проблемы с созданием реверса, забыл как это делать. 
+//Решение создания реверса числа одолжил из интернета, каюсь. Честно говоря, понял плохо, как это работает.
+Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int[] pal = {(int)num};
-void Palindrome(int[] pal)
-{
-    int count = pal.Length;
+int num1 = num;
+string rev = num.ToString();
+char[] ar = rev.ToCharArray();
+Array.Reverse(ar);
+rev = new String(ar);
+num = Convert.ToInt32(rev);
+if(num == num1) Console.WriteLine("Ваше число палиндром");
+else Console.WriteLine("ваше число не палиндром");
 
-    for (int i = 0; i < count/2; i++)
-    {
-       for (int j = count; j > i; j--)
-       {
-            if (i == j) Console.WriteLine("Ваше число палиндром");
-           else  Console.WriteLine("ваше число не палиндром");
-    
-       }
-    }
-}
-
-Palindrome(pal);
